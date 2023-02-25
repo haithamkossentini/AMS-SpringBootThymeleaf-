44ea14209fab27066cf4f7378e956bbf73c9e2a7
haitham kossentini <haitham.kossentini@esprit.tn>
2023-02-25 21:21:12 +0100

    first commit

---
+++ src/main/java/com/sip/ams/CampSpringApplication.java
@@ -0,0 +1,16 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampSpringApplication.class, args);
+		//System.out.println("Couche Data");
+		//Provider p1 = new Provider();
+	   // Provider p2 = new Provider();
+	}
+
+}
---
+++ src/main/java/com/sip/ams/entities/Provider.java
@@ -0,0 +1,70 @@
+package com.sip.ams.entities;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column; 
+import javax.persistence.Entity; 
+import javax.persistence.GeneratedValue; 
+import javax.persistence.GenerationType; 
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Provider {
+	@Id  //clé primaire
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Column(name = "name")
+    private String name;
+    
+    @NotBlank(message = "Address is mandatory")
+    @Column(name = "address")
+    private String address;
+    
+    @NotBlank(message = "Email is mandatory")
+    @Column(name = "email")
+    private String email;
+    
+	@OneToMany(cascade = CascadeType.REMOVE, mappedBy = "provider")
+	private List<Article> articles;
+
+    public Provider() {}
+
+    public Provider(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
